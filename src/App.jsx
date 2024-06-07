727527987cdaf060be3abb658aa4e9f2bfd7f3f3
nilesh7757 <nileshmori7757@gmail.com>
2024-06-07 16:55:25 +0530

    Hi

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,10 @@ function App() {
   return (
     <div className="main w-full h-full relative overflow-y-auto">
     <Navbar/>
-    <div className="bg-blue-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
+    {/* <div className="bg-blue-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"> */}
 
        <Manager/> 
-        </div>
+        {/* </div> */}
     <Footer/>
     </div>
   )
