cf777f51ef7423e2240d06585fc3ff8be7846eff
nilesh7757 <nileshmori7757@gmail.com>
2024-06-07 17:04:26 +0530

    first nilesh

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  output: "export",
-  server: {
-    host: '0.0.0.0', // This allows Vite to listen on all addresses, including your local network
-    port: 3000,
-  },
+  base:'/HI/'
 })
